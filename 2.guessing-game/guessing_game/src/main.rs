e7395f55e78140a19ac8558381454f2abfc9c774
Zerotay <donggun1000@naver.com>
2024-04-30 10:02:30 +0000

    feat: update 2

---
+++ 2.guessing-game/guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Secret number is {secret_number}");
+    println!("Guess the number");
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("type number");
+                continue;
+            }
+        };
+
+        println!("You Guessed {guess}",);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("small"),
+            Ordering::Greater => println!("big"),
+            Ordering::Equal => {
+                println!("win");
+                break;
+            }
+        }
+    }
+}
