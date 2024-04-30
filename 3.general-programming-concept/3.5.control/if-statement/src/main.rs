353e8a320b5a76d0ea6696c41c70d38ab50a73f6
Zerotay <donggun1000@naver.com>
2024-04-30 16:43:21 +0000

    feat: update 3.5

---
+++ 3.general-programming-concept/3.5.control/if-statement/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let number = 3;
+
+    let _what =  if number < 4 {
+        println!("true!!");
+    } else {
+        println!("false!!");
+    };
+
+
+    let cond = true;
+    let number = if cond { 5 } else { 6 };
+    println!("number is {number}");
+
+}
