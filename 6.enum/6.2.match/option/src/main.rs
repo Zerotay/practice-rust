651cbd4511b1bd7c243d957a0476d59a94f5ef15
Zerotay <donggun1000@naver.com>
2024-05-01 07:18:36 +0000

    feat: update 6.2

---
+++ 6.enum/6.2.match/option/src/main.rs
@@ -0,0 +1,20 @@
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+fn main() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => something(),
+        2 => nothing(),
+        _ => (),
+    }
+    fn something() {}
+    fn nothing() {}
+}
